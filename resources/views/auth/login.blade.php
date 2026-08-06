09e120fb2824c0833f5eb994857cfc460224dd9d
Arslan Ikhlaq <arslanikhlaq59@gmail.com>
2026-08-06 11:41:37 +0500

    merge

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,8 +27,8 @@
                     <p class="mt-1 text-sm text-red-600">{{ $message }}</p>
                 @enderror
             </div>
-            <div>
-                
+            <div> 
+
                 <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
                 <input id="password" name="password" type="password" required
                        class="mt-1 w-full rounded border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
